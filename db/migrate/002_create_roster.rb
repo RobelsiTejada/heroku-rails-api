e645e9be11a38b047c7aa0bfbd05ee1ed5ba04ee
RobelsiTejada <robelsitejada@gmail.com>
2017-08-28 11:25:29 -0400

    Adding db:migrate files

--- db/migrate/002_create_roster.rb
+++ db/migrate/002_create_roster.rb
@@ -2,7 +2,7 @@
 
 class CreateRoster < ActiveRecord::Migration
   def change
-    create_table :roster do |t|
+    create_table :rosters do |t|
       t.string :userRoster, index: true, null: false
       t.integer :sportID, null: false
       t.references :user, index: true, foreign_key: true, null: false
