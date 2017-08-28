3cbbd26d531c79d3ebcc93fde54710ea64b4e9f2
RobelsiTejada <robelsitejada@gmail.com>
2017-08-28 13:08:49 -0400

    Adding db:migrate files

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -3,7 +3,6 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.text :username, null: false
       t.string :email, null: false, index: { unique: true }
       t.string :token, null: false, index: { unique: true }
       t.string :password_digest, null: false
