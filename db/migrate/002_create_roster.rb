2e80a2f1e39da4bcf1b455e76aecd6085a349952
RobelsiTejada <robelsitejada@gmail.com>
2017-08-28 11:12:27 -0400

    Adding db:migrate files

---
+++ db/migrate/002_create_roster.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateRoster < ActiveRecord::Migration
+  def change
+    create_table :roster do |t|
+      t.string :userRoster, null: false
+      t.integer :sportID, null: false
+      t.references :user, index: true, foreign_key: true, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
