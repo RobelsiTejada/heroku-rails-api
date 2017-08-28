cde08a8f324905cf2415b4544d23d3b001617aad
RobelsiTejada <robelsitejada@gmail.com>
2017-08-28 11:17:23 -0400

    Adding db:migrate files

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -6,7 +6,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email, null: false, index: { unique: true }
       t.string :token, null: false, index: { unique: true }
       t.string :password_digest, null: false
-      t.string :userRoster, null: false
 
       t.timestamps null: false
     end
