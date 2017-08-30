fefe88e88c15ca9571971f07791317cfac8cb1a5
RobelsiTejada <robelsitejada@gmail.com>
2017-08-30 12:50:39 -0400

    Commit for presentation

--- scripts/get-players.sh
+++ scripts/get-players.sh
@@ -1,11 +1,16 @@
-PI="${API_ORIGIN:-https://api.stattleship.com/baseball/mlb/}"
-URL_PATH="/player_season_stats"
+PI="${API_ORIGIN:-https://api.stattleship.com}"
+URL_PATH="/baseball/mlb/player_season_stats"
   curl "${API}${URL_PATH}" \
     --include \
     --request GET \
     --header "Content-Type: application/json" \
     --header "Authorization: Token token=676decc240402bce97668ada571dd8b3" \
     --header "Accept: application/vnd.stattleship.com; version=1" \
+    --data '{
+       "player_season_stats": {
+      "player_id": "'mlb-david-ortiz'"
+    }
+  }'
 
 
   echo
