b9156399c09607000ff13189609c6abc45d8e083
RobelsiTejada <robelsitejada@gmail.com>
2017-10-03 23:49:49 -0400

    pushing table fix 1

--- db/migrate/005_create_players.rb
+++ db/migrate/005_create_players.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
-
-class CreateGames < ActiveRecord::Migration
+class CreatePlayers < ActiveRecord::Migration
   def change
-    create_table :games do |t|
-      t.text :opponent, null: false
-      t.string :schedule, null: false
+    create_table :players do |t|
+      t.text :text, null: false
       t.references :league, index: true, foreign_key: true, null: false
+      t.references :game, index: true, foreign_key: true, null: false
       t.references :roster, index: true, foreign_key: true, null: false
       t.references :user, index: true, foreign_key: true, null: false
 
--- scripts/players.sh
+++ scripts/players.sh
@@ -1,22 +1,10 @@
-API="${API_ORIGIN:-https://api.stattleship.com}"
-URL_PATH="/football/nfl/player_season_stats"
-  curl "${API}${URL_PATH}" \
+API="${API_ORIGIN:-https://newsapi.org/v1/articles?source=techcrunch&apiKey=49a804e7a8fb430fbb153a7528609102}"
+  curl "${API}" \
     --include \
     --request GET \
     --header "Content-Type: application/json" \
     --header "Authorization: Token token=676decc240402bce97668ada571dd8b3" \
-    --header "Accept: application/vnd.stattleship.com; version=1" \
-    --data '{
-       "player_season_stats": {
-         "per_page": "40"
-         "page":
-         "season_id": "'"${SEASON_ID}"'"
-         "team_id": "'"${TEAM_ID}"'"
-         "player_id": "'"${PLAYER_ID}"'"
-         "interval_type":
-         "on":
-    }
-  }'
+    --data
 
 
   echo
