3159cf8e8428e2dde40c1da7a98f01f06d871df4
RobelsiTejada <robelsitejada@gmail.com>
2017-10-05 19:54:42 -0400

    pushing table fix 6

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :yahoo_auth, ENV['YAHOO_APP_ID'], ENV['YAHOO_APP_SECRET']
   { name: 'yahoo',
-    # redirect_uri: 'https://robelsitejada.github.io/fsl',
-    redirect_uri: 'http://website.dev/callbackurl/for/app',
+    redirect_uri: 'https://mysterious-beyond-27378.herokuapp.com/for/app',
     image_size: '96x96' }
 end
